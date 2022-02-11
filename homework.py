905b1b735c49cc6a8cb973779bc9374409e48fce
Стас Басов <stas.basov@whatever.com>
2022-02-11 18:24:38 +0500

    Вынесен текстовый шаблон в константу

--- homework.py
+++ homework.py
@@ -11,12 +11,12 @@ class InfoMessage:
     calories: float
 
     def get_message(self):
-        return (f'Тип тренировки: {self.training_type};'
-                f' Длительность: {self.duration:.3f} ч.;'
-                f' Дистанция: {self.distance:.3f} км;'
-                f' Ср. скорость: {self.speed:.3f} км/ч;'
-                f' Потрачено ккал: {self.calories:.3f}.'
-                )
+        info_get_message: str = (f'Тип тренировки: {self.training_type};'
+                                 f' Длительность: {self.duration:.3f} ч.;'
+                                 f' Дистанция: {self.distance:.3f} км;'
+                                 f' Ср. скорость: {self.speed:.3f} км/ч;'
+                                 f' Потрачено ккал: {self.calories:.3f}.')
+        return info_get_message.format(**asdict(self))
 
 
 class Training:
